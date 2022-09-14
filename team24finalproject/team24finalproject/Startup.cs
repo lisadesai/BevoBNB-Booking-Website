2a7b0ce110ae10669d9f6cc1a14b9a4f681d4771
lisa <74734294+lisadesai@users.noreply.github.com>
2022-09-14 17:22:49 -0500

    Add files via upload

---
+++ team24finalproject/team24finalproject/Startup.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Globalization;
+
+//: (For HW1 and HW2) Delete the below statements.
+//: (For HW3 and beyond) Uncomment these statements.
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+
+
+//: (For HW1) Delete the below statement. 
+//: (For HW2 and beyond) Update this using statement to reference your project 
+using Team24_Final_Project.Models;
+
+
+//: (For HW1 and HW2) Delete the below statement.
+//: (For HW3 and beyond) Update this using statement to reference your project 
+using Team24_Final_Project.DAL;
+
+
+//: Make this namespace match your project - be sure to remove the []
+namespace Team24_Final_Project
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //NOTE: This adds the MVC engine and Razor code
+            services.AddControllersWithViews();
+
+            //: (For HW3 and beyond) Add a connection string here once you have created it on Azure
+            String connectionString = "Server=tcp:fa21team24finalproject1.database.windows.net,1433;Initial Catalog=fa21team24finalproject;Persist Security Info=False;User ID=MISAdmin;Password=Password1;MultipleActiveResultSets=True;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+            //: (For HW3 and beyond) Uncomment this line once you have your connection string
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
+
+            services.AddIdentity<AppUser, IdentityRole>(opts => {
+                opts.User.RequireUniqueEmail = true;
+                opts.Password.RequiredLength = 6;
+                opts.Password.RequireNonAlphanumeric = false;
+                opts.Password.RequireLowercase = false;
+                opts.Password.RequireUppercase = false;
+                opts.Password.RequireDigit = false;
+            })
+            .AddEntityFrameworkStores<AppDbContext>()
+            .AddDefaultTokenProviders();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            //These lines allow you to see more detailed error messages
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+
+            //This line allows you to use static pages like style sheets and images
+            app.UseStaticFiles();
+
+            //This marks the position in the middleware pipeline where a routing decision
+            //is made for a URL.
+            app.UseRouting();
+
+            //This allows the data annotations for currency to work on Macs
+            app.Use(async (context, next) =>
+            {
+                CultureInfo.CurrentCulture = System.Globalization.CultureInfo.CreateSpecificCulture("en-US");
+                CultureInfo.CurrentUICulture = CultureInfo.CurrentCulture;
+
+                await next.Invoke();
+            });
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+
+            //This method maps the controllers and their actions to a patter for
+            //requests that's known as the default route. This route identifies
+            //the Home controller as the default controller and the Index() action
+            //method as the default action. The default route also identifies a 
+            //third segment of the URL that's a parameter named id.
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
